e6b081d47815145b9135c8f8e0c533c5b0118e94
Andre Carrera <acarrera94@gmail.com>
2017-12-13 17:50:30 +0000

    finished binding

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,9 +6,19 @@ myApp.controller("mainController", ["$scope","$firebaseArray",
 function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child("days");
     $scope.days = $firebaseArray(ref);
-    
+    console.log($scope.days);
+
 
     $scope.reset = function() {
+
+    }
+    $scope.book = function(day){
+      //console.log($scope.days);
+      console.log(day);
+      //$scope.days[day.name].slot.booked = true;
+      //$scope.days[day.name] = day;
+      $scope.days.$save(day);
+      console.log($scope.days[day.name]);
 
     }
 }]);
