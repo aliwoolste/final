27c67c124b5f2a1ae15f3ed9f95555f3e3997ab6
Jaxon Keeler <jdk_08_09@yahoo.com>
2017-12-13 16:35:27 +0000

    days in firebase

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,100 +1,14 @@
 // create our angular module and inject firebase
 //angular.module('scheduleApp', ['firebase'])
-var myApp = angular.module("scheduleApp",[]);
+var myApp = angular.module("scheduleApp",["firebase"]);
 // create our main controller and get access to firebase
-myApp.controller("mainController", ["$scope",
-function($scope) {
-$scope.days= {
-  friday: {
-    name: 'Friday',
-    slots: {
-      900: {
-        time: '9:00am',
-        booked: false
-      },
-      0110: {
-        time: '11:00am',
-        booked: false
-      },
-      100: {
-        time: '1:00pm',
-        booked: false
-      },
-      300: {
-        time: '3:00pm',
-        booked: false
-      },
-      500: {
-        time: '5:00pm',
-        booked: false
-      },
-      700: {
-        time: '7:00pm',
-        booked: false
-      }
-    }
-  },
-  saturday: {
-    name: 'Saturday',
-    slots: {
-      900: {
-        time: '9:00am',
-        booked: false
-      },
-      0110: {
-        time: '11:00am',
-        booked: false
-      },
-      100: {
-        time: '1:00pm',
-        booked: false
-      },
-      300: {
-        time: '3:00pm',
-        booked: false
-      },
-      500: {
-        time: '5:00pm',
-        booked: false
-      },
-      700: {
-        time: '7:00pm',
-        booked: false
-      }
-    }
-  },
-  sunday: {
-    name: 'Sunday',
-    slots: {
-      900: {
-        time: '9:00am',
-        booked: false
-      },
-      0110: {
-        time: '11:00am',
-        booked: false
-      },
-      100: {
-        time: '1:00pm',
-        booked: false
-      },
-      300: {
-        time: '3:00pm',
-        booked: false
-      },
-      500: {
-        time: '5:00pm',
-        booked: false
-      },
-      700: {
-        time: '7:00pm',
-        booked: false
-      }
-    }
-  }
-};
+myApp.controller("mainController", ["$scope","$firebaseArray",
+function($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child("days");
+    $scope.days = $firebaseArray(ref);
+    
 
-$scope.reset = function() {
+    $scope.reset = function() {
 
-}
+    }
 }]);
