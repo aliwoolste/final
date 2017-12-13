f95e0461281b52473ba642caa84e0cb53324c97e
Jaxon Keeler <jdk_08_09@yahoo.com>
2017-12-13 19:31:07 +0000

    blarg

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,8 @@ var myApp = angular.module("scheduleApp",["firebase"]);
 myApp.controller("mainController", ["$scope","$firebaseArray",
 function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child("days");
+    var refEmail = firebase.database().ref().child("email");
+    $scope.emails = $firebaseArray(refEmail);
     $scope.days = $firebaseArray(ref);
     console.log($scope.days);
 
@@ -18,6 +20,12 @@ function($scope, $firebaseArray) {
       //$scope.days[day.name] = day;
       $scope.days.$save(day);
       console.log($scope.days[day.name]);
-
     }
+
+    $scope.saveEmail = function(email) {
+        console.log("Email from save email f(x)" +email);
+        $scope.emails.$add(email);
+    }
+
+
 }]);
